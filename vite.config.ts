507f21f65a400aa83bfbb1914e5fe05ab64fdc72
ymnlkn <yamanalkin9@gmail.com>
2025-06-21 21:43:04 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { tempo } from "tempo-devtools/dist/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    entries: ["src/main.tsx", "src/tempobook/**/*"],
+  },
+  plugins: [react(), tempo()],
+  resolve: {
+    preserveSymlinks: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    // @ts-ignore
+    allowedHosts: true,
+  },
+});
